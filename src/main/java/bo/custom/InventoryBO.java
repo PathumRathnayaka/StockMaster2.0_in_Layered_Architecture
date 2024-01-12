9173a0493b12c9fc4ea58787f8fb7a64c53f637c
PathumRathnayaka <135231247+PathumRathnayaka@users.noreply.github.com>
2024-01-13 01:53:37 +0530

    newUpdate

--- src/main/java/bo/custom/InventoryBO.java
+++ src/main/java/bo/custom/InventoryBO.java
@@ -1,4 +1,19 @@
 package bo.custom;
 
-public interface InventoryBO {
+
+
+import bo.SuperBO;
+import dto.InventoryDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface InventoryBO extends SuperBO {
+    ArrayList<InventoryDto> getAllInventory() throws SQLException, ClassNotFoundException;
+    boolean saveInventory(InventoryDto dto) throws SQLException, ClassNotFoundException;
+    boolean updateInventory(InventoryDto dto) throws SQLException, ClassNotFoundException;
+    boolean existInventory(String id) throws SQLException, ClassNotFoundException;
+    void deleteInventory(String id) throws SQLException, ClassNotFoundException;
+    String generateInventory() throws SQLException, ClassNotFoundException;
+    InventoryDto searchInventory(String id) throws SQLException, ClassNotFoundException;
 }
--- src/main/java/bo/custom/impl/InventoryBOImpl.java
+++ src/main/java/bo/custom/impl/InventoryBOImpl.java
@@ -1,4 +1,54 @@
 package bo.custom.impl;
 
-public class InventoryBOImpl {
+import bo.custom.InventoryBO;
+import dao.DAOFactory;
+import dao.custom.InventoryDAO;
+import dto.InventoryDto;
+import entity.Inventory;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class InventoryBOImpl implements InventoryBO {
+    InventoryDAO inventoryDAO=(InventoryDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.INVENTORY);
+    @Override
+    public ArrayList<InventoryDto> getAllInventory() throws SQLException, ClassNotFoundException {
+        ArrayList<Inventory> inventories=inventoryDAO.getAll();
+        ArrayList<InventoryDto> itemDtos=new ArrayList<>();
+        for (Inventory inventory:inventories) {
+            itemDtos.add(new InventoryDto(inventory.getInventoryId(),inventory.getItemID(),inventory.getGodownID(),inventory.getTrackID(),inventory.getInvoiceNumber(),inventory.getQuantity(),inventory.getDate()));
+        }
+        return null;
+    }
+
+    @Override
+    public boolean saveInventory(InventoryDto dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean updateInventory(InventoryDto dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean existInventory(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public void deleteInventory(String id) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public String generateInventory() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public InventoryDto searchInventory(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
